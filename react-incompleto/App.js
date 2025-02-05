5f9d70ac4c8ea1c0135e264716075ac71ecaf996
sumrya <moratogabriela931@gmail.com>
2025-02-05 16:38:54 -0300

    React (ainda não finalizado)

---
+++ react-incompleto/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+function App() {
+  return (
+    <Footer></Footer>
+  );
+}
+
+export default App;
